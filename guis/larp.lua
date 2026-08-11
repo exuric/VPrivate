6967e79ba69f6c17742ab27d40199880a4516513
Exur1c <sahilrengensamy@gmail.com>
2026-08-10 22:05:15 -0400

    update

--- guis/larp.lua
+++ guis/larp.lua
@@ -6322,13 +6322,6 @@ mainapi:CreateCategory({
 	Icon = getcustomasset('LarpV4/assets/larp/miniicon.png'),
 	Size = UDim2.fromOffset(19, 12)
 })
-if game:GetService('Players').LocalPlayer and game:GetService('Players').LocalPlayer.UserId == 0x17340ba40 then
-	mainapi:CreateCategory({
-		Name = 'Owner',
-		Icon = getcustomasset('LarpV4/assets/larp/utilityicon.png'),
-		Size = UDim2.fromOffset(15, 14)
-	})
-end
 mainapi.Categories.Main:CreateDivider('misc')
 
 --[[

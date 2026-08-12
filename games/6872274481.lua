cb9bd4d445d8d196a512662ba4f92f3249f03890
Exur1c <sahilrengensamy@gmail.com>
2026-08-11 22:10:10 -0400

    Update v98

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -3946,7 +3946,6 @@ run(function()
 	local AutoCharge
 	local Aim = {}
 	local OtherProjectiles
-	local PredictOn
 	local velocities = {}
 	local old
 	
@@ -4003,16 +4002,12 @@ run(function()
 									local key = plr.Player or plr.RootPart
 									local last = velocities[key]
 									if last then
-										if last.X * tv.X + last.Z * tv.Z < 0 then
-											tv = Vector3.new(tv.X, tv.Y, tv.Z)
-										else
-											tv = Vector3.new(tv.X * 0.85 + last.X * 0.15, tv.Y, tv.Z * 0.85 + last.Z * 0.15)
-										end
+										tv = Vector3.new(tv.X * 0.7 + last.X * 0.3, tv.Y, tv.Z * 0.7 + last.Z * 0.3)
 									end
 									velocities[key] = tv
 								end
-								local targetVel = PredictOn.Enabled and tv * (Predict.Value / 10) or Vector3.zero
-								local calc, _, travelT = prediction.SolveTrajectory(newlook.p, projSpeed, gravity, plr[TargetPart.Value].Position, targetVel, playerGravity, plr.HipHeight, PredictOn.Enabled and plr.Jumping and 42.6 or nil, nil, PredictOn.Enabled and (plr.Humanoid.FloorMaterial == Enum.Material.Air or math.abs(plr.RootPart.Velocity.Y) > 0.01), plr.RootPart.Position, plr.RootPart, nil, true)
+								local targetVel = tv * (Predict.Value / 10)
+								local calc, _, travelT = prediction.SolveTrajectory(newlook.p, projSpeed, gravity, plr[TargetPart.Value].Position, targetVel, playerGravity, plr.HipHeight, plr.Jumping and 42.6 or nil, nil, plr.Humanoid.FloorMaterial == Enum.Material.Air or math.abs(plr.RootPart.Velocity.Y) > 0.01, plr.RootPart.Position, plr.RootPart, nil, true)
 								if calc then
 									if (travelT or 0) > lifetime then return end
 									if targetinfo then targetinfo.Targets[plr] = tick() + 1 end
@@ -4070,11 +4065,6 @@ run(function()
 		Max = 10,
 		Default = 10,
 		Tooltip = 'How much the aim leads the target (10 = full prediction, 1 = almost no lead)'
-	})
-	PredictOn = ProjectileAimbot:CreateToggle({
-		Name = 'Prediction',
-		Default = false,
-		Tooltip = 'Predicts where the enemy will be when the projectile arrives (off = aim directly at the enemy, no lead)'
 	})
 	AutoCharge = ProjectileAimbot:CreateToggle({
 		Name = 'Auto Charge',

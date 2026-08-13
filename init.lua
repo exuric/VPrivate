7aef1cf85e92afda1754cf61b879bf64a73ae8b0
Exur1c <sahilrengensamy@gmail.com>
2026-08-13 05:18:24 -0400

    Update v109

--- init.lua
+++ init.lua
@@ -73,7 +73,7 @@ if not shared.LarpDeveloper then
 	local commit = 'main'
 	local stored = isfile('LarpV4/profiles/commit.txt') and readfile('LarpV4/profiles/commit.txt') or ''
 	local version = isfile('LarpV4/.version') and readfile('LarpV4/.version') or ''
-	if commit ~= stored or version ~= '108' then
+	if commit ~= stored or version ~= '109' then
 		if stored ~= '' and stored ~= commit then
 			shared.updated = stored
 		end
@@ -89,7 +89,7 @@ if not shared.LarpDeveloper then
 			end
 		end
 	end
-	writefile('LarpV4/.version', '108')
+	writefile('LarpV4/.version', '109')
 	writefile('LarpV4/profiles/commit.txt', commit)
 	if #listfiles('LarpV4/profiles') < 4 then
 		shared.LarpPresetInstall = function()

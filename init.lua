94b368c8e599e1c56f0ffb9b163c6d3b1366b5c1
Exur1c <sahilrengensamy@gmail.com>
2026-08-16 14:57:16 -0400

    update

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(ga
 
 local RTOK = ''
 local ROOT = (RTOK ~= '' and 'https://'..RTOK..'@' or 'https://')..'raw.githubusercontent.com/exuric/VPrivate/'
-local SELFCOMMIT = 'ac062a4bf19339971ecf7d7e8a0389d7fb217be6'
+local SELFCOMMIT = '607e8f791fe7f7059d2fb3fb88e51c0f1b2ed2d1'
 local LARPWATER = '--LARP:'..SELFCOMMIT..'\n'
 
 local function downloadFile(path, func)

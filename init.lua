630e5c5228149ac0c3b5e0d46343626f6c20652f
Exur1c <sahilrengensamy@gmail.com>
2026-08-18 01:36:43 -0400

    harden the whitelist against local forgery and webhook spoofing

--- init.lua
+++ init.lua
@@ -102,6 +102,121 @@ do
 	end
 end
 
+local bit32_band, bit32_bxor = bit32.band, bit32.bxor
+local bit32_lshift, bit32_rshift = bit32.lshift, bit32.rshift
+local bit32_lrotate, bit32_rrotate = bit32.lrotate, bit32.rrotate
+local TWO56_POW_7 = 256 ^ 7
+local common_W = {}
+local sha2_K_lo = {3609767458,602891725,3964484399,2173295548,4081628472,3053834265,2937671579,3664609560,2734883394,1164996542,1323610764,3590304994,4068182383,991336113,633803317,3479774868,2666613458,944711139,2341262773,2007800933,1495990901,1856431235,3175218132,2198950837,3999719339,766784016,2566594879,3203337956,1034457026,2466948901,3758326383,168717936,1188179964,1546045734,1522805485,2643833823,2343527390,1014477480,1206759142,344077627,1290863460,3158454273,3505952657,106217008,3606008344,1432725776,1467031594,851169720,3100823752,1363258195,3750685593,3785050280,3318307427,3812723403,2003034995,3602036899,1575990012,1125592928,2716904306,442776044,593698344,3733110249,2999351573,3815920427,3928383900,566280711,3454069534,4000239992,1914138554,2731055270,3203993006,320620315,587496836,1086792851,365543100,2618297676,3409855158,4234509866,987167468,1246189591}
+local sha2_K_hi = {1116352408,1899447441,3049323471,3921009573,961987163,1508970993,2453635748,2870763221,3624381080,310598401,607225278,1426881987,1925078388,2162078206,2614888103,3248222580,3835390401,4022224774,264347078,604807628,770255983,1249150122,1555081692,1996064986,2554220882,2821834349,2952996808,3210313671,3336571891,3584528711,113926993,338241895,666307205,773529912,1294757372,1396182291,1695183700,1986661051,2177026350,2456956037,2730485921,2820302411,3259730800,3345764771,3516065817,3600352804,4094571909,275423344,430227734,506948616,659060556,883997877,958139571,1322822218,1537002063,1747873779,1955562222,2024104815,2227730452,2361852424,2428436474,2756734187,3204031479,3329325298,3391569614,3515267271,3940187606,4118630271,116418474,174292421,289380356,460393269,685471733,852142971,1017036298,1126000580,1288033470,1501505948,1607167915,1816402316}
+local sha2_H_lo = {4089235720,2227873595,4271175723,1595750129,2917565137,725511199,4215389547,327033209}
+local sha2_H_hi = {1779033703,3144134277,1013904242,2773480762,1359893119,2600822924,528734635,1541459225}
+local function sha512_feed_128(H_lo, H_hi, str, offs, size)
+	local W, K_lo, K_hi = common_W, sha2_K_lo, sha2_K_hi
+	local h1_lo, h2_lo, h3_lo, h4_lo, h5_lo, h6_lo, h7_lo, h8_lo = H_lo[1], H_lo[2], H_lo[3], H_lo[4], H_lo[5], H_lo[6], H_lo[7], H_lo[8]
+	local h1_hi, h2_hi, h3_hi, h4_hi, h5_hi, h6_hi, h7_hi, h8_hi = H_hi[1], H_hi[2], H_hi[3], H_hi[4], H_hi[5], H_hi[6], H_hi[7], H_hi[8]
+	for pos = offs, offs + size - 1, 128 do
+		for j = 1, 16 * 2 do
+			pos = pos + 4
+			local a, b, c, d = string.byte(str, pos - 3, pos)
+			W[j] = ((a * 256 + b) * 256 + c) * 256 + d
+		end
+		for jj = 34, 160, 2 do
+			local a_lo, a_hi, b_lo, b_hi = W[jj - 30], W[jj - 31], W[jj - 4], W[jj - 5]
+			local tmp1 = bit32_bxor(bit32_rshift(a_lo, 1) + bit32_lshift(a_hi, 31), bit32_rshift(a_lo, 8) + bit32_lshift(a_hi, 24), bit32_rshift(a_lo, 7) + bit32_lshift(a_hi, 25)) % 4294967296 +
+				bit32_bxor(bit32_rshift(b_lo, 19) + bit32_lshift(b_hi, 13), bit32_lshift(b_lo, 3) + bit32_rshift(b_hi, 29), bit32_rshift(b_lo, 6) + bit32_lshift(b_hi, 26)) % 4294967296 +
+				W[jj - 14] + W[jj - 32]
+			local tmp2 = tmp1 % 4294967296
+			W[jj - 1] = bit32_bxor(bit32_rshift(a_hi, 1) + bit32_lshift(a_lo, 31), bit32_rshift(a_hi, 8) + bit32_lshift(a_lo, 24), bit32_rshift(a_hi, 7)) +
+				bit32_bxor(bit32_rshift(b_hi, 19) + bit32_lshift(b_lo, 13), bit32_lshift(b_hi, 3) + bit32_rshift(b_lo, 29), bit32_rshift(b_hi, 6)) +
+				W[jj - 15] + W[jj - 33] + (tmp1 - tmp2) / 4294967296
+			W[jj] = tmp2
+		end
+		local a_lo, b_lo, c_lo, d_lo, e_lo, f_lo, g_lo, h_lo = h1_lo, h2_lo, h3_lo, h4_lo, h5_lo, h6_lo, h7_lo, h8_lo
+		local a_hi, b_hi, c_hi, d_hi, e_hi, f_hi, g_hi, h_hi = h1_hi, h2_hi, h3_hi, h4_hi, h5_hi, h6_hi, h7_hi, h8_hi
+		for j = 1, 80 do
+			local jj = 2 * j
+			local tmp1 = bit32_bxor(bit32_rshift(e_lo, 14) + bit32_lshift(e_hi, 18), bit32_rshift(e_lo, 18) + bit32_lshift(e_hi, 14), bit32_lshift(e_lo, 23) + bit32_rshift(e_hi, 9)) % 4294967296 +
+				(bit32_band(e_lo, f_lo) + bit32_band(-1 - e_lo, g_lo)) % 4294967296 +
+				h_lo + K_lo[j] + W[jj]
+			local z_lo = tmp1 % 4294967296
+			local z_hi = bit32_bxor(bit32_rshift(e_hi, 14) + bit32_lshift(e_lo, 18), bit32_rshift(e_hi, 18) + bit32_lshift(e_lo, 14), bit32_lshift(e_hi, 23) + bit32_rshift(e_lo, 9)) +
+				bit32_band(e_hi, f_hi) + bit32_band(-1 - e_hi, g_hi) +
+				h_hi + K_hi[j] + W[jj - 1] +
+				(tmp1 - z_lo) / 4294967296
+			h_lo = g_lo
+			h_hi = g_hi
+			g_lo = f_lo
+			g_hi = f_hi
+			f_lo = e_lo
+			f_hi = e_hi
+			tmp1 = z_lo + d_lo
+			e_lo = tmp1 % 4294967296
+			e_hi = z_hi + d_hi + (tmp1 - e_lo) / 4294967296
+			d_lo = c_lo
+			d_hi = c_hi
+			c_lo = b_lo
+			c_hi = b_hi
+			b_lo = a_lo
+			b_hi = a_hi
+			tmp1 = z_lo + (bit32_band(d_lo, c_lo) + bit32_band(b_lo, bit32_bxor(d_lo, c_lo))) % 4294967296 + bit32_bxor(bit32_rshift(b_lo, 28) + bit32_lshift(b_hi, 4), bit32_lshift(b_lo, 30) + bit32_rshift(b_hi, 2), bit32_lshift(b_lo, 25) + bit32_rshift(b_hi, 7)) % 4294967296
+			a_lo = tmp1 % 4294967296
+			a_hi = z_hi + (bit32_band(d_hi, c_hi) + bit32_band(b_hi, bit32_bxor(d_hi, c_hi))) + bit32_bxor(bit32_rshift(b_hi, 28) + bit32_lshift(b_lo, 4), bit32_lshift(b_hi, 30) + bit32_rshift(b_lo, 2), bit32_lshift(b_hi, 25) + bit32_rshift(b_lo, 7)) + (tmp1 - a_lo) / 4294967296
+		end
+		a_lo = h1_lo + a_lo
+		h1_lo = a_lo % 4294967296
+		h1_hi = (h1_hi + a_hi + (a_lo - h1_lo) / 4294967296) % 4294967296
+		a_lo = h2_lo + b_lo
+		h2_lo = a_lo % 4294967296
+		h2_hi = (h2_hi + b_hi + (a_lo - h2_lo) / 4294967296) % 4294967296
+		a_lo = h3_lo + c_lo
+		h3_lo = a_lo % 4294967296
+		h3_hi = (h3_hi + c_hi + (a_lo - h3_lo) / 4294967296) % 4294967296
+		a_lo = h4_lo + d_lo
+		h4_lo = a_lo % 4294967296
+		h4_hi = (h4_hi + d_hi + (a_lo - h4_lo) / 4294967296) % 4294967296
+		a_lo = h5_lo + e_lo
+		h5_lo = a_lo % 4294967296
+		h5_hi = (h5_hi + e_hi + (a_lo - h5_lo) / 4294967296) % 4294967296
+		a_lo = h6_lo + f_lo
+		h6_lo = a_lo % 4294967296
+		h6_hi = (h6_hi + f_hi + (a_lo - h6_lo) / 4294967296) % 4294967296
+		a_lo = h7_lo + g_lo
+		h7_lo = a_lo % 4294967296
+		h7_hi = (h7_hi + g_hi + (a_lo - h7_lo) / 4294967296) % 4294967296
+		a_lo = h8_lo + h_lo
+		h8_lo = a_lo % 4294967296
+		h8_hi = (h8_hi + h_hi + (a_lo - h8_lo) / 4294967296) % 4294967296
+	end
+	H_lo[1], H_lo[2], H_lo[3], H_lo[4], H_lo[5], H_lo[6], H_lo[7], H_lo[8] = h1_lo, h2_lo, h3_lo, h4_lo, h5_lo, h6_lo, h7_lo, h8_lo
+	H_hi[1], H_hi[2], H_hi[3], H_hi[4], H_hi[5], H_hi[6], H_hi[7], H_hi[8] = h1_hi, h2_hi, h3_hi, h4_hi, h5_hi, h6_hi, h7_hi, h8_hi
+end
+local function inlineSha512(message)
+	local length, tail = 0, ''
+	local H_lo, H_hi = {}, {}
+	for j = 1, 8 do H_lo[j] = sha2_H_lo[j]; H_hi[j] = sha2_H_hi[j] end
+	local partLength = #message
+	length = length + partLength
+	local size = partLength
+	local size_tail = size % 128
+	sha512_feed_128(H_lo, H_hi, message, 0, size - size_tail)
+	tail = tail .. string.sub(message, partLength + 1 - size_tail)
+	local final_blocks = {tail, '\128', string.rep('\0', (-17 - length) % 128 + 9)}
+	tail = nil
+	length = length * (8 / TWO56_POW_7)
+	for j = 4, 10 do
+		length = length % 1 * 256
+		final_blocks[j] = string.char(math.floor(length))
+	end
+	final_blocks = table.concat(final_blocks)
+	sha512_feed_128(H_lo, H_hi, final_blocks, 0, #final_blocks)
+	local out = {}
+	for j = 1, 8 do
+		out[j] = string.format('%08x', H_hi[j] % 4294967296) .. string.format('%08x', H_lo[j] % 4294967296)
+	end
+	return table.concat(out)
+end
+
 local function fileDigest(path)
 	local content = readfile(path)
 	local i = content:find('\n')
@@ -117,6 +232,17 @@ local function fileDigest(path)
 end
 
 local function verifyFiles()
+	local ok, good = pcall(function()
+		local content = readfile('LarpV4/libraries/hash.lua')
+		local i = content:find('\n')
+		if i then
+			content = content:sub(i + 1)
+		end
+		return inlineSha512(content) == MANIFEST['libraries/hash.lua']
+	end)
+	if not ok or not good then
+		pcall(delfile, 'LarpV4/libraries/hash.lua')
+	end
 	hash = loadstring(downloadFile('LarpV4/libraries/hash.lua'), 'hash')()
 	for _, path in VERIFY_FILES do
 		local full = 'LarpV4/'..path

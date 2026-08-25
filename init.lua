368a751df2e82b8c36ee576f67c91eff6f7bdbe2
Exur1c <sahilrengensamy@gmail.com>
2026-08-25 12:08:45 -0400

    bump SELFCOMMIT to ff81a681991d05f373c2894d03382b94147dd6f5

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(ga
 local RTOK = ''
 local BRANCH = 'test'
 local ROOT = (RTOK ~= '' and 'https://'..RTOK..'@' or 'https://')..'raw.githubusercontent.com/exuric/VPrivate/'
-local SELFCOMMIT = '91a1b59602fb421e5ade362f817a777b5401b7ea'
+local SELFCOMMIT = 'ff81a681991d05f373c2894d03382b94147dd6f5'
 
 local function fetchCommit()
 	local ok, res = pcall(function()

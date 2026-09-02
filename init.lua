3f7bf56be170d7110a9e36bbb3cd0e8c6034e635
Exuric <dev@exuric.com>
2026-09-02 18:11:32 -0400

    update

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(ga
 local RTOK = ''
 local BRANCH = 'main'
 local ROOT = (RTOK ~= '' and 'https://'..RTOK..'@' or 'https://')..'raw.githubusercontent.com/exuric/VPrivate/'
-local SELFCOMMIT = 'c2f2ab2916decb607a88add583b53414e4f55387'
+local SELFCOMMIT = 'bd07ca6a1bd3a838c752863b93202d6eb2033df3'
 
 local function fetchCommit()
 	local ok, res = pcall(function()

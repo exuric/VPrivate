b911a6efd01a8abd4c82994c4e0dddc7948e626a
sahil <lollitanarsayah9@gmail.com>
2026-09-19 20:30:35 -0400

    Revert "nametags: fix SecondaryBars scope (was declared in wrong module)"
    This reverts commit 609fbe40f65197afa73fc6f73daece997148b1fb.

--- games/8560631822.lua
+++ games/8560631822.lua
@@ -5862,6 +5862,7 @@ run(function()
 	local Reference = {}
 	local Folder = Instance.new('Folder')
 	Folder.Parent = larp.gui
+	local SecondaryBars = {}
 	
 	local function Added(bed)
 		if not BedESP.Enabled then return end
@@ -6781,7 +6782,6 @@ run(function()
 	local Strings, Sizes, Reference = {}, {}, {}
 	local Folder = Instance.new('Folder')
 	Folder.Parent = larp.gui
-	local SecondaryBars = {}
 	local methodused
 	local DeviceCache = {}
 
